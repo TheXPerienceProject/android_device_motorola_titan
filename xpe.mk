84d2b49b19595e9ab9a80888e202fd6ab1216fbe
mestreian <ghenrique737@gmail.com>
2016-11-23 10:04:59 -0200

    XPEify

---
+++ xpe.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/motorola/titan/full_titan.mk)
+
+# Inherit some common XPE stuff.
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G 2014
+PRODUCT_NAME := xpe_titan
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
